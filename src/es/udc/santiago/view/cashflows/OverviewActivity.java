4fef3e2196d480b0c9d65aa23eab3865b11829fa
Santiago Munín <santiago@Felurian.(none)>
2012-05-30 01:34:07 +0200

    First functional version of movements overview 	* res/layout/movements_overview.xml: Added monthly and yearly layout. 	* src/es/udc/santiago/view/cashflows/OverviewActivity.java: Added date picker.

--- src/es/udc/santiago/view/cashflows/OverviewActivity.java
+++ src/es/udc/santiago/view/cashflows/OverviewActivity.java
@@ -1,15 +1,22 @@
 package es.udc.santiago.view.cashflows;
 
 import java.sql.SQLException;
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TextView;
 
 import com.j256.ormlite.android.apptools.OrmLiteBaseTabActivity;
@@ -28,6 +35,20 @@ import es.udc.santiago.model.facade.Period;
  * 
  */
 public class OverviewActivity extends OrmLiteBaseTabActivity<DatabaseHelper> {
+	private static final String TAG = "OverviewActivity";
+	private static final int DATE_PICKER_DIALOG = 1;
+
+	private DatePickerDialog.OnDateSetListener dateListener = new DatePickerDialog.OnDateSetListener() {
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			day = new GregorianCalendar(year, monthOfYear, dayOfMonth);
+			dayButton.setText(DateFormat.getDateInstance()
+					.format(day.getTime()));
+			new GetMovementsTask().execute((Void) null);
+		}
+	};
+
 	TabHost mTabHost;
 	Button dayButton;
 	Calendar day;
@@ -39,42 +60,112 @@ public class OverviewActivity extends OrmLiteBaseTabActivity<DatabaseHelper> {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.movements_overview);
-
-		mTabHost = getTabHost();
-
-		mTabHost.addTab(mTabHost.newTabSpec("tab_test1")
-				.setIndicator(getString(R.string.daily))
-				.setContent(R.id.dailytab));
-		mTabHost.addTab(mTabHost.newTabSpec("tab_test2")
-				.setIndicator(getString(R.string.monthly))
-				.setContent(R.id.monthlytab));
-		mTabHost.addTab(mTabHost.newTabSpec("tab_test3")
-				.setIndicator(getString(R.string.yearly))
-				.setContent(R.id.yearlytab));
-
-		mTabHost.setCurrentTab(0);
-
-		day = Calendar.getInstance();
-		period = Period.ONCE;
 		try {
 			cashService = new CashFlowService(getHelper());
 		} catch (SQLException e) {
 		}
+		mTabHost = getTabHost();
+
+		mTabHost.addTab(mTabHost.newTabSpec("daily")
+				.setIndicator(getString(R.string.daily))
+				.setContent(R.id.dailytab));
+		mTabHost.addTab(mTabHost.newTabSpec("monthly")
+				.setIndicator(getString(R.string.monthly))
+				.setContent(R.id.monthlytab));
+		mTabHost.addTab(mTabHost.newTabSpec("yearly")
+				.setIndicator(getString(R.string.yearly))
+				.setContent(R.id.yearlytab));
+		mTabHost.setOnTabChangedListener(new OnTabChangeListener() {
+
+			@Override
+			public void onTabChanged(String tabId) {
+				if (day == null) {
+					day = Calendar.getInstance();
+				}
+				if (tabId.equals("daily")) {
+					period = Period.ONCE;
+					dayButton = (Button) findViewById(R.id.daybutton);
+					dayButton.setText(DateFormat.getDateInstance().format(
+							day.getTime()));
+					incomes = (TextView) findViewById(R.id.incomes_daily);
+					spends = (TextView) findViewById(R.id.spends_daily);
+					balance = (TextView) findViewById(R.id.balance_daily);
+				}
+				if (tabId.equals("monthly")) {
+					period = Period.MONTHLY;
+					dayButton = (Button) findViewById(R.id.monthbutton);
+					dayButton.setText(DateFormat.getDateInstance().format(
+							day.getTime()));
+					incomes = (TextView) findViewById(R.id.incomes_monthly);
+					spends = (TextView) findViewById(R.id.spends_monthly);
+					balance = (TextView) findViewById(R.id.balance_monthly);
+				}
+				if (tabId.equals("yearly")) {
+					period = Period.YEARLY;
+					dayButton = (Button) findViewById(R.id.yearbutton);
+					dayButton.setText(DateFormat.getDateInstance().format(
+							day.getTime()));
+					incomes = (TextView) findViewById(R.id.incomes_yearly);
+					spends = (TextView) findViewById(R.id.spends_yearly);
+					balance = (TextView) findViewById(R.id.balance_yearly);
+
+				}
+				new GetMovementsTask().execute((Void) null);
+			}
+		});
+		mTabHost.setCurrentTab(0);
+		day = Calendar.getInstance();
+		period = Period.ONCE;
+
+		new GetMovementsTask().execute((Void) null);
+
 		dayButton = (Button) findViewById(R.id.daybutton);
 		dayButton.setText(getString(R.string.today));
 		dayButton.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				// TODO date dialog
+				showDialog(DATE_PICKER_DIALOG);
+			}
+		});
+		dayButton = (Button) findViewById(R.id.monthbutton);
+		dayButton.setText(getString(R.string.today));
+		dayButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog(DATE_PICKER_DIALOG);
+			}
+		});
+		dayButton = (Button) findViewById(R.id.yearbutton);
+		dayButton.setText(getString(R.string.today));
+		dayButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog(DATE_PICKER_DIALOG);
 			}
 		});
 		incomes = (TextView) findViewById(R.id.incomes_daily);
 		spends = (TextView) findViewById(R.id.spends_daily);
 		balance = (TextView) findViewById(R.id.balance_daily);
+	}
 
-		GetMovementsTask gTask = new GetMovementsTask();
-		gTask.execute((Void[]) null);
+	/**
+	 * Date picker's dialogs
+	 */
+	protected Dialog onCreateDialog(int id) {
+		Calendar d = GregorianCalendar.getInstance();
+
+		switch (id) {
+		case DATE_PICKER_DIALOG:
+			return new DatePickerDialog(this, dateListener,
+					d.get(Calendar.YEAR), d.get(Calendar.MONTH),
+					d.get(Calendar.DATE));
+
+		}
+
+		return null;
 	}
 
 	/**
@@ -88,6 +179,7 @@ public class OverviewActivity extends OrmLiteBaseTabActivity<DatabaseHelper> {
 
 		@Override
 		protected List<CashFlow> doInBackground(Void... params) {
+			Log.i(TAG, "Fetching movements");
 			return cashService.getAllWithFilter(day, period, null, null);
 		}
 
